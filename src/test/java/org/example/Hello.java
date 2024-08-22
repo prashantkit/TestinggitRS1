da5ff06ee312f486aeb4b34d5916da40e1014180
prashant shrivastava <prashantshrivastava@prashants-MBP.lan>
2024-08-22 05:58:35 -0500

    Initial commit

---
+++ src/test/java/org/example/Hello.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class Hello {
+    @Test
+    public void testjebkins(){
+        System.out.println( "hello Jenkins");
+    }
+}
